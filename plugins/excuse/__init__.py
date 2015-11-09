0a032c8840ecd979d8f0762eb44224c0413cfc69
BuildTools <louis@sortofaweso.me>
2015-11-09 13:16:45 -0800

    convert to string for tls transport

--- plugins/excuse/__init__.py
+++ plugins/excuse/__init__.py
@@ -6,4 +6,4 @@ helptext = "Gives a lazy developer excuse"
 def do(args, pipe=None):
     r = requests.get("http://developerexcuses.com")
     soup = BeautifulSoup(r.text, 'html.parser')
-    return soup.find_all('a')[0].get_text()
+    return str(soup.find_all('a')[0].get_text())
